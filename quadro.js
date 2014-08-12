096ba271a96b3879ca61386af6d9a607b56bc226
Gabriel Seralha Miró <gsmiro@gmail.com>
2014-08-12 00:33:55 -0300

    Cleanup

--- quadro.js
+++ quadro.js
@@ -1,15 +1,6 @@
 #!/usr/bin/env node
 var db = require("level")('./quadro.db');
 
-var qd = function(){
+module.exports = function(){
 
 }
-qd.exports.people = function(obj,cb){
-  if(typeof obj === undefined)return db.get("people",cb);
-  if(typeof obj === 'string'){
-    obj = require(obj);
-  }
-
-  return db.put("people",obj,cb);
-}
-qd.exports.rules = 
